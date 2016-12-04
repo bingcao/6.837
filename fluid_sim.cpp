4cf796f5915c6fc7667c42e9eb1ebb80388f9fee
Bingfei <tonycao96@gmail.com>
2016-12-04 14:40:08 -0500

    LOL

---
+++ fluid_sim.cpp
@@ -0,0 +1,13 @@
+//
+//  fluid_sim.cpp
+//  project
+//
+//  Created by Tony Cao on 12/4/16.
+//  Copyright © 2016 Tony Cao. All rights reserved.
+//
+
+#include "fluid_sim.hpp"
+
+void advect() {
+  
+}
--- project/main.cpp
+++ project/main.cpp
@@ -8,13 +8,41 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <GLUT/glut.h>
 #include "MAC.h"
 #include "vecmath.h"
+#include <glfw3.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
   // insert code here...
-  Vector2f a(0, 0);
-  a.print();
+  GLFWwindow* window;
+  /* Initialize the library */
+  if (!glfwInit())
+    return -1;
+  
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+  
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+  
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+    
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+  
+  glfwTerminate();
   return 0;
 }
