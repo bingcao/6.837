a10f54a216f835a64859b1a5f6c5996ba4b6fbef
Bingfei <tonycao96@gmail.com>
2016-12-04 12:39:52 -0500

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Tony Cao on 12/4/16.
+//  Copyright © 2016 Tony Cao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
